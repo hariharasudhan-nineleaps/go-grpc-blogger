45ab460527a715c2bcd510ff326329f150232694
Hariharasudhan <hari@nineleaps.com>
2023-05-24 15:14:42 +0530

    comment functionality.

--- compile.sh
+++ compile.sh
@@ -9,3 +9,4 @@ find ./ -type f -name "*pb.go" | xargs rm -rf
 protoc --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative server/grpc/proto/auth/auth.proto
 protoc --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative server/grpc/proto/user/user.proto
 protoc --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative server/grpc/proto/blog/blog.proto
+protoc --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative server/grpc/proto/comment/comment.proto
